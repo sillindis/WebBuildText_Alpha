aeec4c63be58f6f19d82a60e300f452ecaceaa7b
DevBlackWolf <blackwolfdev1223@gmail.com>
2025-02-20 20:08:11 +0900

    수정

---
+++ TemplateData/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-숏폼형 퀴즈 솔루션 왓퀴즈!-25.02.20.001";
+const contentToCache = [
+    "Build/TemplateData.loader.js",
+    "Build/TemplateData.framework.js.unityweb",
+    "Build/TemplateData.data.unityweb",
+    "Build/TemplateData.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
