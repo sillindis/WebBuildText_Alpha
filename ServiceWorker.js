a329e86a3719dcdef4b6886be013be8e67f075dc
DevBlackWolf <blackwolfdev1223@gmail.com>
2025-02-21 10:35:43 +0900

    수정

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,35 @@
 const cacheName = "DefaultCompany-숏폼형 퀴즈 솔루션 왓퀴즈!-25.02.21.001";
 const contentToCache = [
     "Build/WebBuildText_Alpha.loader.js",
-    "Build/WebBuildText_Alpha.framework.js",
-    "Build/WebBuildText_Alpha.data",
-    "Build/WebBuildText_Alpha.wasm",
+    "Build/WebBuildText_Alpha.framework.js.unityweb",
+    "Build/WebBuildText_Alpha.data.unityweb",
+    "Build/WebBuildText_Alpha.wasm.unityweb",
     "TemplateData/style.css"
-
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
+    // 기존 캐시 삭제
+    e.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cache) {
+                    return caches.delete(cache);
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
+    // 네트워크 우선 전략으로 변경
+    e.respondWith(
+        fetch(e.request)
+            .then(function(response) {
+                return response;
+            })
+            .catch(function() {
+                return caches.match(e.request);
+            })
+    );
 });
